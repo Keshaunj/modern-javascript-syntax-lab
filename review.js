421e4c06c90004d07b0087893219a075720faefd
Keshaunj <keshaunjones48@gmail.com>
2024-10-21 17:36:55 -0400

    done

--- review.js
+++ review.js
@@ -110,9 +110,77 @@ console.log(propertyName)
 
 // 1. `cat`
 
+
+// Cats('dog','fluffy')
+
 // 2. `white`
 
 // The function should log a sentence 'The cat is white.' by default. The function should substitute the appropriate parameters when supplied arguments.
 
 // Your code here
+const Cats = (noun ="The cat",adjective = "is white")=>
+  { 
+    console.log(noun ,adjective)
 
+}
+Cats()
+
+
+
+
+///////9
+// Convert the following `if...else` statement in to a ternary:
+
+const pizza = 'tasty';
+
+if (pizza === 'tasty') {
+  console.log('yum');
+} else {
+  console.log('yuck');
+}
+
+// Your code here
+
+
+
+
+let pizzas = 'tasty';
+
+pizza === 'tasty' ?   console.log('yum') : console.log('yuck');
+ 
+
+///////////////10
+
+
+// 1. SET LANGUAGE
+
+// Construct a single line of code that assigns a default value using the logical OR operator. This line should match the logic of the following statement: 
+
+// "LANG is equal to localLangConfig or the default value of English."
+
+const localLangConfig = 'en';  // Change to 'es', 'fr', etc., or keep it null
+
+// a. Create a variable called LANG
+// b. Assign LANG the value of localLangConfig or 'en' as a default
+
+// Your code here
+const LANG = localLangConfig || 'fr';
+// Log the result
+console.log('Language setting:', LANG);
+
+
+
+
+/////////////11
+
+// 2. SET WEBSITE THEME
+
+const userSavedTheme = 'dark'; // Change to 'dark', 'contrast', etc., or keep it null
+
+// a. Create a variable called USER_THEME
+// b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
+
+// Your code here
+const USER_THEME = userSavedTheme || 'light'
+// Log the result
+console.log('User theme setting:', USER_THEME);
